05c8a63296b87c6a967a81781b80c0268c019920
Ainun Hafni <ainunlia64@gmail.com>
2016-10-15 14:59:06 +0700

    designok

---
+++ app/src/main/java/id/sch/smktelkom_mlg/tuga01/xiirpl3003/psbsekolah/MainActivity.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.tuga01.xiirpl3003.psbsekolah;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/id/sch/smktelkom_mlg/tuga01/xiirpl3003/psbsekolah/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package id.sch.smktelkom_mlg.tuga01.xiirpl3003.psbsekolah;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
